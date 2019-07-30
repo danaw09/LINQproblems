f9fcab2703eb9e0d8f8ab6bf039f9bff1eb1312a
dana <danaw09@gmail.com>
2019-07-30 11:48:29 -0500

    add commit

--- linqProblems/linqProblems/Class1.cs
+++ linqProblems/linqProblems/Class1.cs
@@ -9,22 +9,41 @@ namespace linqProblems
     class Class1
     {
         
-        List<string> words = new List<string>() { "the", "bike", "this", "it", "tenth", “mathematics” };
+        List<string> words = new List<string>() { "the", "bike", "this", "it", "tenth", "mathematics" };
         List<string> names = new List<string>() { "Mike", "Brad", "Nevin", "Ian", "Mike" };
         private object listOfStrings;
+        List<string> classGrades = new List<string>() { "90,100,82,89,55", "80,100,92,89,65", "93,81,78,84,69", "73,88,83,99,64", "98,100,66,74,55" };
+        public double classAverageGrade;
 
-        public List<string> addStringToList(List <string> list)
+        public Class1()
         {
 
         }
-       
 
-        public List<string> RemoveDuplicatesFromStringList(List<string> list)
+        public void PrintListToBeProcessed(List<string> words)
         {
-            var listOfStringsWithOutDuplicates = listOfStrings.Distinct().ToList();
-            string delimiter = ", ";
-            Console.WriteLine("Here is the new list: " + listOfStringsWithOutDuplicates.Aggregate((i, j) => i + delimiter + j) + ".\n");
-            return listOfStringsWithOutDuplicates;
+
+            var wordsWithTH = words.Any(w => words.Contains(w));
+
+        }
+
+        public void ReturnStringNoDUplicate(List<string> names)
+        {
+
+            var wordsNoDuplicate = names.Distinct().ToList();
+
+        }
+
+        public void getClassAverage()
+        {
+            var temporaryListOfGrades = classGrades.OrderBy(g => g);
+        }
+        
+        public static void AverageClassScore(List<string> classGrades)
+        {
+            List<int> classGrades = new List<int>();
+            double classAverage = 0;
+            int numberInClass = 0;
         }
 
     }
