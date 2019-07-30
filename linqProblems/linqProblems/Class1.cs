f92f3652df4096dc3a829fecfeffc946ad306f99
dana <danaw09@gmail.com>
2019-07-30 10:15:07 -0500

    added print function to class

--- linqProblems/linqProblems/Class1.cs
+++ linqProblems/linqProblems/Class1.cs
@@ -8,5 +8,24 @@ namespace linqProblems
 {
     class Class1
     {
+        
+        List<string> words = new List<string>() { "the", "bike", "this", "it", "tenth", “mathematics” };
+        List<string> names = new List<string>() { "Mike", "Brad", "Nevin", "Ian", "Mike" };
+        private object listOfStrings;
+
+        public List<string> addStringToList(List <string> list)
+        {
+
+        }
+       
+
+        public List<string> RemoveDuplicatesFromStringList(List<string> list)
+        {
+            var listOfStringsWithOutDuplicates = listOfStrings.Distinct().ToList();
+            string delimiter = ", ";
+            Console.WriteLine("Here is the new list: " + listOfStringsWithOutDuplicates.Aggregate((i, j) => i + delimiter + j) + ".\n");
+            return listOfStringsWithOutDuplicates;
+        }
+
     }
 }
